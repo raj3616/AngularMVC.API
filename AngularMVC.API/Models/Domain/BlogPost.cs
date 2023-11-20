b84fe67d628badcf1df8553ee331ee2a7b10c996
raj <rajg3616@gmail.com>
2023-11-20 22:09:47 +0530

    Changes  are making file slow that why i removed

--- AngularMVC.API/Models/Domain/BlogPost.cs
+++ AngularMVC.API/Models/Domain/BlogPost.cs
@@ -11,6 +11,5 @@
         public DateTime PublishedDate { get; set; }
         public string Author { get; set; }
         public bool IsVisible { get; set; }
-        public bool IsVisible1 { get; set; }
     }
 }
